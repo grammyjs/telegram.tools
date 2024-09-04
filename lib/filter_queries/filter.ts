072cac5ba70b3b7280fa0086a04dcbddb042e9c9
Roj <ez@roj.im>
2024-09-04 16:05:08 +0300

    Update filter queries

--- lib/filter_queries/filter.ts
+++ lib/filter_queries/filter.ts
@@ -3,6 +3,10 @@ export const UPDATE_KEYS = [
   "edited_message",
   "channel_post",
   "edited_channel_post",
+  "business_connection",
+  "business_message",
+  "edited_business_message",
+  "deleted_business_messages",
   "inline_query",
   "chosen_inline_result",
   "callback_query",
@@ -13,18 +17,22 @@ export const UPDATE_KEYS = [
   "my_chat_member",
   "chat_member",
   "chat_join_request",
+  "message_reaction",
+  "message_reaction_count",
+  "chat_boost",
+  "removed_chat_boost",
 ];
 
 export const L1_SHORTCUTS = {
   "": ["message", "channel_post"],
-  msg: ["message", "channel_post"],
-  edit: ["edited_message", "edited_channel_post"],
+  "msg": ["message", "channel_post"],
+  "edit": ["edited_message", "edited_channel_post"],
 };
 
 export const L2_SHORTCUTS = {
   "": ["entities", "caption_entities"],
-  media: ["photo", "video"],
-  file: [
+  "media": ["photo", "video"],
+  "file": [
     "photo",
     "animation",
     "audio",
